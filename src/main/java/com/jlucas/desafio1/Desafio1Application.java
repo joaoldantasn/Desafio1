0d5e7799814e2d617a2f0099e81341b5797153e8
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-06-06 18:12:54 -0300

    desafio 1 completo

---
+++ src/main/java/com/jlucas/desafio1/Desafio1Application.java
@@ -0,0 +1,28 @@
+package com.jlucas.desafio1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jlucas.desafio1.entities.Order;
+import com.jlucas.desafio1.services.OrderService;
+
+@SpringBootApplication
+public class Desafio1Application implements CommandLineRunner {
+	
+	@Autowired
+	private OrderService orderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Order order = new Order(1309, 95.90, 0.0);
+		System.out.println("Pedido código " + order.getCode());
+		System.out.println("Valor total: " + orderService.total(order));
+	}
+
+}
